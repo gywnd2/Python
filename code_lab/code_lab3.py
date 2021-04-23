5b31582024ea31441b4a0f27a11f0c9807c6979d
Hyo-jung Lee <leeexpert@cau.ac.kr>
2021-04-23 19:41:55 +0900

    210423

---
+++ code_lab/code_lab3.py
@@ -0,0 +1,14 @@
+"""
+2021-1 Signals and systems
+code_lab
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+x=np.arange(10)
+y=np.random.rand(10)
+
+plt.plot(x, y)
+plt.show()
